9c2dbf66e518528312c6ea1835a78c4d12161e18
Kristof Mattei <864376+Kristof-Mattei@users.noreply.github.com>
2022-02-14 16:55:30 -0800

    test: broken test

--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -11,3 +11,10 @@ fn assert_world_ok2() {
     let cls2 = || false;
     assert_eq!(cls1(), cls2());
 }
+
+#[test]
+fn assert_world_ok3() {
+    let cls1 = || false;
+    let cls2 = || true;
+    assert_eq!(cls1(), cls2());
+}
